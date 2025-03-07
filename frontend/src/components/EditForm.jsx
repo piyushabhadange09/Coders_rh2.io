5d510ad18b4c7ab48261bfdad25d4eb48c206240
piyusha05 <piyushabhadange9@gmail.com>
2025-03-07 17:04:04 +0530

    Error in EditForm.jsx- React is never used

--- frontend/src/components/EditForm.jsx
+++ frontend/src/components/EditForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import  { useState } from "react"
 import { Typography } from "@mui/material"
 import Alert from "@mui/material/Alert"
 import Box from "@mui/material/Box"
